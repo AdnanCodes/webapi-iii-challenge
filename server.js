71c2920729bd3dcab8b176303be086209632fb3c
Adnan <adnan.chowdhury@outlook.com>
2019-09-04 16:39:58 -0500

    logger built

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const usersRouter = require("./users/userRouter");
 
 const server = express();
 server.use(express.json());
+
+function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} ${req.method} to ${req.url}`);
+  next();
+}
+server.use(logger);
 server.use("/users", usersRouter);
+
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
-
 module.exports = server;
