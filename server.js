a89a2051a9ea3b72842e54a1c4814906649fac77
Adnan <adnan.chowdhury@outlook.com>
2019-09-04 16:15:35 -0500

    Project Setup, Routing Linked, Implemented 2 endpoints

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = 'express';
+const express = require("express");
+const usersRouter = require("./users/userRouter");
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+server.use("/users", usersRouter);
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,55 @@
-const express = 'express';
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post("/", validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(() =>
+      res
+        .status(500)
+        .json({ errorMessage: "Couldn't store the user in the database" })
+    );
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {});
 
+router.get("/", (req, res) => {
+  Users.get()
+    .then(user => res.status(200).json(user))
+    .catch(() => res.status(500).json({ error: "Couldn't retrieve Users" }));
 });
 
-router.get('/', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
-};
+function validateUserId(req, res, next) {}
 
 function validateUser(req, res, next) {
+  function isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  }
+  if (isEmpty(req.body)) {
+    res.status(400).json({ errorMessage: "Missing a body" });
+  } else if (req.body.name === undefined) {
+    res.status(400).json({ errorMessage: "Missing a name Field" });
+  } else {
+    next();
+  }
+}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
