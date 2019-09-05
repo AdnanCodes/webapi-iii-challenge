9d3384046995750151211c051e2e535c97b03fe7
Adnan <adnan.chowdhury@outlook.com>
2019-09-05 14:31:24 -0500

    Config for Heroku Deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
+require("dotenv").config();
 const server = require("./server");
 
-const port = 8000;
+const port = process.env.PORT;
 
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
